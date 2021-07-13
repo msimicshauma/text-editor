304457f50421c0dca5750e4d540cb0530c5938a5
msimic <milos@ultimatemovers.net>
2021-07-13 12:18:25 +0200

    extract menu to separate component and css cleanup

---
+++ src/components/Menu.vue
@@ -0,0 +1,175 @@
+<template>
+  <div class="menu-wrapper">
+    <span class="menu-header">Text Layer</span>
+    <hr class="break">
+    <div class="flex">
+      <div class="big-input">
+        <label for="elementText">Enter text</label>
+        <input
+          v-model="newElementText"
+          type="text"
+          id="elementText"
+          name="elementText"
+          placeholder="Text for new element"
+        >
+      </div>
+      <div class="plus-button">
+        +
+      </div>
+    </div>
+    <div class="flex justify-between">
+      <div class="small-input">
+        <label for="fontSize">Font size</label>
+        <input
+          v-model="fontSize"
+          type="number"
+          id="fontSize"
+          name="fontSize"
+          placeholder="Font size (px)"
+        >
+      </div>
+      <div class="small-input">
+        <label for="lineHeight">Line height</label>
+        <input
+          v-model="lineHeight"
+          type="number"
+          id="lineHeight"
+          name="lineHeight"
+          placeholder="Line height (px)"
+        >
+      </div>
+    </div>
+    <div class="option-container">
+      <label for="fontFamily">Change font family</label>
+      <select
+        v-model="fontFamily"
+        id="fontFamily"
+      >
+        <option value="roboto">Roboto</option>
+      </select>
+      <img
+        src="/src/assets/icons/caret-down.svg"
+        class="caret-down"
+      >
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Menu",
+    data() {
+      return {
+        newElementText: '',
+        fontSize: 12,
+        lineHeight: 1.5,
+        fontFamily: ''
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  /* Flexbox classes */
+  .flex {
+    display: flex;
+  }
+  .justify-between {
+    justify-content: space-between;
+  }
+
+  /* Menu wrapper and header */
+  .menu-wrapper {
+    font-family: 'Arial', sans-serif;
+    background-color: #fff;
+    padding: 25px 20px;
+    width: 250px;
+  }
+  .menu-header {
+    color: #797979;
+    font-weight: 600;
+    font-size: 18px;
+    padding-bottom: 11px;
+  }
+
+  /* Border that separates header and menu items */
+  .break {
+    background-color: #e5e5e5;
+    border: 0;
+    height: 2px;
+    margin: 12px 0;
+  }
+
+  /* Custom classes for inputs */
+  .big-input {
+    display: flex;
+    flex-direction: column;
+    flex-grow: 1;
+  }
+  .small-input {
+    display: flex;
+    flex-direction: column;
+    margin-top: 25px;
+    width: 115px;
+  }
+
+  /* Input and label defaults */
+  label {
+    font-size: 14px;
+    color: #5d5d5d;
+  }
+  input {
+    height: 19px;
+    padding: 0 5px;
+    border: 2px solid #e5e5e5;
+    border-radius: 5px;
+    outline: none;
+    margin-top: 5px;
+  }
+  input::-webkit-outer-spin-button,
+  input::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+  input[type=number] {
+    -moz-appearance: textfield;
+  }
+
+  /* Plus button */
+  .plus-button {
+    height: 23px;
+    width: 23px;
+    line-height: 23px;
+    text-align: center;
+    vertical-align: middle;
+    border-radius: 5px;
+    align-self: flex-end;
+    cursor: pointer;
+    background-color: #3ac888;
+    color: #fff;
+    font-size: 19px;
+    font-weight: bold;
+    margin-left: 15px;
+  }
+
+  /* Options */
+  .option-container {
+    display: flex;
+    flex-direction: column;
+    margin-top: 25px;
+    position: relative;
+  }
+  select {
+    appearance: none;
+    border: 2px solid #e5e5e5;
+    padding: 3px 5px;
+    border-radius: 5px;
+    outline: none;
+    margin-top: 5px;
+  }
+  .caret-down {
+    position: absolute;
+    bottom: 0;
+    right: 0;
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,70 +7,23 @@
       <div class="canvas-wrapper">
         Canvas
       </div>
-      <div class="menu-wrapper">
-        <span class="menu-header">Text Layer</span>
-        <hr class="break">
-        <div class="flex justify-between">
-          <div class="flex flex-col flex-grow">
-            <label for="entertext">Enter text</label>
-            <input type="text" id="entertext" name="entertext">
-          </div>
-          <div class="plus-button">
-            +
-          </div>
-        </div>
-        <div class="flex justify-between">
-          <div class="flex flex-col width-115 margin-top-25">
-            <label for="fontsize">Font size</label>
-            <input type="text" id="fontsize" name="fontsize">
-          </div>
-          <div class="flex flex-col width-115 margin-top-25">
-            <label for="lineheight">Line height</label>
-            <input type="text" id="lineheight" name="lineheight">
-          </div>
-        </div>
-        <div class="flex flex-col margin-top-25 relative">
-          <label for="cars">Change font family</label>
-          <select
-            id="cars"
-            class="margin-top-5"
-          >
-            <option value="roboto">Roboto</option>
-          </select>
-          <img
-            src="/src/assets/icons/caret-down.svg"
-            class="caret-down"
-          >
-        </div>
-      </div>
+      <Menu />
     </div>
   </div>
 </template>
 
 <script>
+  import Menu from "../components/Menu"
+
   export default {
     name: 'Home',
-    components: {}
+    components: {
+      Menu
+    }
   }
 </script>
 
 <style scoped>
-  .flex {
-    display: flex;
-  }
-  .flex-col {
-    flex-direction: column;
-  }
-  .flex-grow {
-    flex-grow: 1;
-  }
-  .justify-between {
-    justify-content: space-between;
-  }
-  .relative {
-    position: relative;
-  }
-
   .wrapper {
     display: flex;
     flex-direction: column;
@@ -97,71 +50,5 @@
     justify-content: center;
     align-items: center;
     flex-grow: 1;
-  }
-  .menu-wrapper {
-    font-family: 'Arial', sans-serif;
-    background-color: #fff;
-    padding: 25px 20px;
-    width: 250px;
-  }
-  .menu-header {
-    color: #797979;
-    font-weight: 600;
-    font-size: 18px;
-    padding-bottom: 11px;
-  }
-  .break {
-    background-color: #e5e5e5;
-    border: 0;
-    height: 2px;
-    margin: 12px 0;
-  }
-  label {
-    font-size: 14px;
-    color: #5d5d5d;
-  }
-  input {
-    height: 19px;
-    padding: 0 5px;
-    border: 2px solid #e5e5e5;
-    border-radius: 5px;
-    outline: none;
-    margin-top: 5px;
-  }
-  .width-115 {
-    width: 115px;
-  }
-  .margin-top-5 {
-    margin-top: 5px;
-  }
-  .margin-top-25 {
-    margin-top: 25px;
-  }
-  .plus-button {
-    height: 23px;
-    width: 23px;
-    line-height: 23px;
-    text-align: center;
-    vertical-align: middle;
-    border-radius: 5px;
-    align-self: flex-end;
-    cursor: pointer;
-    background-color: #3ac888;
-    color: #fff;
-    font-size: 19px;
-    font-weight: bold;
-    margin-left: 15px;
-  }
-  select {
-    appearance: none;
-    border: 2px solid #e5e5e5;
-    padding: 3px 5px;
-    border-radius: 5px;
-    outline: none;
-  }
-  .caret-down {
-    position: absolute;
-    bottom: 0;
-    right: 0;
   }
 </style>
